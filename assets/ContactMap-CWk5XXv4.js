0e82862213629a377d18c889bef509d32e42f70a
lekovvv-cmd <anton202004@gmail.com>
2025-06-16 02:56:24 -0700

    Updates

---
+++ assets/ContactMap-CWk5XXv4.js
@@ -0,0 +1,6 @@
+import{c as r}from"./createLucideIcon-mv5X1tYN.js";import{j as e}from"./index-C2CC2HuG.js";/**
+ * @license lucide-react v0.511.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const c=[["path",{d:"M20 10c0 4.993-5.539 10.193-7.399 11.799a1 1 0 0 1-1.202 0C9.539 20.193 4 14.993 4 10a8 8 0 0 1 16 0",key:"1r0f0z"}],["circle",{cx:"12",cy:"10",r:"3",key:"ilqhr7"}]],o=r("map-pin",c),s=()=>e.jsx("div",{className:"w-full h-full",children:e.jsx("iframe",{src:"https://yandex.ru/map-widget/v1/?um=constructor%3A88d1366521035d387bed4408aea14a98fef1f16bbc1babfd088571bdfbced5b6&source=constructor",title:"Яндекс.Карта — г. Москва, ул. Пушкина 9, 1/3",className:"w-full h-full",frameBorder:"0",allowFullScreen:!0})});export{s as C,o as M};
---
+++ assets/contact-doctor-BkRBA16w.js
@@ -0,0 +1,1 @@
+const t="/clinic-site/assets/contact-doctor-BbFqaeNC.webp";export{t};
